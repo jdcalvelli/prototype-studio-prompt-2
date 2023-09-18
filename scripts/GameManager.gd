6a6cd5d96f718a366692c3b7ee0b8b4f622a23d3
JD Calvelli <jdcalvelli@jds-macbook-air.lan>
2023-09-18 12:34:07 -0400

    adding the holding keys logic

--- scripts/GameManager.gd
+++ scripts/GameManager.gd
@@ -1,10 +1,28 @@
 extends Node
 
-# Called when the node enters the scene tree for the first time.
+var keyboardKeys = [
+	KEY_Q, KEY_W, KEY_E, KEY_R, KEY_T, KEY_Y, KEY_U, KEY_O, KEY_P,
+	KEY_A, KEY_S, KEY_D, KEY_F, KEY_G, KEY_H, KEY_J, KEY_K, KEY_L,
+	KEY_Z, KEY_X, KEY_C, KEY_V, KEY_B, KEY_N, KEY_M,
+]
+
+var selectedKeyboardKeys = []
+
+var numKeysToSelect = 5
+
 func _ready():
-	pass # Replace with function body.
+	# this is to pick non repeating
+	var n = 0
+	while n < numKeysToSelect:
+		var keyToAdd = keyboardKeys.pick_random()
+		if selectedKeyboardKeys.has(keyToAdd):
+			continue
+		else:
+			selectedKeyboardKeys.append(keyToAdd)
+			n += 1
+	print(selectedKeyboardKeys)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	for key in selectedKeyboardKeys:
+		if Input.is_key_pressed(key):
+			print(str(key) + " is held")
